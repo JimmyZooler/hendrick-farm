57eea51f45efbbe47125e1d7782617220123ed10
Craig <jimmy.zooler@gmail.com>
2017-07-19 18:31:45 -0400

    Lifestyle page complete with animations on hover.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,6 +9,7 @@
         $document = $(document),
         $window = $(window),
         $body = $("body");
+    
     $document.scroll(function() {
         if ($window.width() >= 991 ) {
             if ($document.scrollTop() >= 6 ) {
@@ -40,10 +41,10 @@
             $indicatorTwo = $('#indicator-2'),
             $indicatorThree = $('#indicator-3'),
             $indicatorFour = $('#indicator-4'),
+            $indicatorFive = $('#indicator-5'),
             $heroPager = $('.hero--pager');
         
-        var waypoint = new Waypoint({
-          element: $('#hero-section-4'),
+        $('#hero-section-4').waypoint({
           handler: function(direction) {
               if ( direction == 'down' ) {
                   $navbar.addClass("zero-opacity");
@@ -54,8 +55,7 @@
           offset: 'bottom-in-view'
         });
         
-        var homeFiveWaypoint = new Waypoint({
-          element: $('#home-section-5'),
+        $('#home-section-5').waypoint({
           handler: function(direction) {
               if ( direction == 'down' ) {
                   $heroPager.addClass("zero-opacity");
@@ -66,8 +66,7 @@
           offset: 'bottom-in-view'
         });
         
-        var pagerOne = new Waypoint({
-          element: $('#hero-section-2'),
+        $('#hero-section-2').waypoint({
           handler: function(direction) {
               if ( direction == 'down' ) {
                   $indicatorOne.removeClass("active");
@@ -80,8 +79,7 @@
             offset: 68
         });
         
-        var pagerTwo = new Waypoint({
-          element: $('#hero-section-3'),
+        $('#hero-section-3').waypoint({
           handler: function(direction) {
               if ( direction == 'down' ) {
                   $indicatorTwo.removeClass("active");
@@ -94,8 +92,7 @@
             offset: 68
         });
         
-        var pagerThree = new Waypoint({
-          element: $('#hero-section-4'),
+        $('#hero-section-4').waypoint({
           handler: function(direction) {
               if ( direction == 'down' ) {
                   $indicatorThree.removeClass("active");
@@ -108,28 +105,92 @@
             offset: 68
         });
         
-        //Linking the dots to each section
+        //
+        // Waypoints for Lifestyle Page
+        //
         
-        $(".pager--indicator-link").click( function() {
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: $( $.attr(this, 'href') ).offset().top - 68
-            }, 1200);
+        $('#life-section-2').waypoint({
+          handler: function(direction) {
+              if ( direction == 'down' ) {
+                  $indicatorOne.removeClass("active");
+                  $indicatorTwo.addClass("active");
+              } else {
+                  $indicatorTwo.removeClass("active");
+                  $indicatorOne.addClass("active");
+              }
+          },
+            offset: 68
         });
+        $('#life-section-3').waypoint({
+          handler: function(direction) {
+              if ( direction == 'down' ) {
+                  $indicatorTwo.removeClass("active");
+                  $indicatorThree.addClass("active");
+              } else {
+                  $indicatorThree.removeClass("active");
+                  $indicatorTwo.addClass("active");
+              }
+          },
+            offset: 68
+        });
+        $('#life-section-4').waypoint({
+          handler: function(direction) {
+              if ( direction == 'down' ) {
+                  $indicatorThree.removeClass("active");
+                  $indicatorFour.addClass("active");
+              } else {
+                  $indicatorFour.removeClass("active");
+                  $indicatorThree.addClass("active");
+              }
+          },
+            offset: 68
+        });
+        $('#life-section-5').waypoint({
+          handler: function(direction) {
+              if ( direction == 'down' ) {
+                  $indicatorFour.removeClass("active");
+                  $indicatorFive.addClass("active");
+              } else {
+                  $indicatorFive.removeClass("active");
+                  $indicatorFour.addClass("active");
+              }
+          },
+            offset: 68
+        });
+        
+//        $('#wrapper-footer').waypoint({
+//          handler: function(direction) {
+//              if ( direction == 'down' ) {
+//                  $heroPager.addClass("zero-opacity");
+//              } else {
+//                  $heroPager.removeClass("zero-opacity");
+//              }
+//          },
+//            offset: '-80%'
+//        });
         
     });
     
-    // scripts for home animations 
+    // scripts for hero-section animations 
     
     $(document).ready(function(){
             var $heroContent2 = $('#hero-section-2 .hero-content'),
                 $heroContent3 = $('#hero-section-3 .hero-content'),
-                $heroContent4 = $('#hero-section-4 .hero-content');
+                $heroContent4 = $('#hero-section-4 .hero-content'),
+                $heroContent5 = $('#life-section-1 .hero-content'),
+                $heroContent6 = $('#life-section-2 .hero-content'),
+                $heroContent7 = $('#life-section-3 .hero-content'),
+                $heroContent8 = $('#life-section-4 .hero-content'),
+                $heroContent9 = $('#life-section-5 .hero-content');
         
           // hide our element on page load
           $heroContent2.css('opacity', 0);
           $heroContent3.css('opacity', 0);
           $heroContent4.css('opacity', 0);
+          $heroContent6.css('opacity', 0);
+          $heroContent7.css('opacity', 0);
+          $heroContent8.css('opacity', 0);
+          $heroContent9.css('opacity', 0);
 
           $heroContent2.waypoint(function() {
               $heroContent2.addClass('fadeInUp');
@@ -140,14 +201,45 @@
         $heroContent4.waypoint(function() {
               $heroContent4.addClass('fadeInUp'); 
           }, { offset: '70%' });
+        $heroContent6.waypoint(function() {
+              $heroContent6.addClass('fadeInUp'); 
+          }, { offset: '70%' });
+        $heroContent7.waypoint(function() {
+              $heroContent7.addClass('fadeInUp'); 
+          }, { offset: '70%' });
+        $heroContent8.waypoint(function() {
+              $heroContent8.addClass('fadeInUp'); 
+          }, { offset: '70%' });
+        $heroContent9.waypoint(function() {
+              $heroContent9.addClass('fadeInUp'); 
+          }, { offset: '70%' });
 
     });
     
     $(document).ready(function(){
+        
+        //Linking the dots to each section
+        
+        $(".pager--indicator-link").click( function() {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: $( $.attr(this, 'href') ).offset().top - 68
+            }, 1200);
+        });
          
         $('.dropdown').addClass('disabled');
         $('.dropdown-toggle').removeAttr('data-toggle');
+        
+        // Scripts for hover background zoom
+        $('.life-link').mouseover( function() {
+            $(this).closest('.life-section').addClass('hover');
+        });
+        $('.life-link').mouseleave( function() {
+            $(this).closest('.life-section').removeClass('hover');
+        });
 
     });
+    
+    
   
 })( jQuery );
--- template-lifestyle.php
+++ template-lifestyle.php
@@ -10,34 +10,54 @@
 
 get_header(); ?>
 
-<div class="hero-section" id="life-section-1" style="background-image: url('<?php the_field('life_image_1') ?>'); ">
-    <div class="hero-content">
+<div class="hero-section life-section" id="life-section-1">
+    <div class="sect-background" style="background-image: url('<?php the_field('life_image_1') ?>'); "></div>
+    <div class="hero-content animated fadeInUp">
         <h2><?php the_field('life_title_1'); ?></h2>
-        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/arrow_white.svg" /></a>
+        <div>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_white.svg" />
+        </div>
+        <a class="life-link" href="#"></a>
     </div>
 </div>
-<div class="hero-section" id="life-section-2" style="background-image: url('<?php the_field('life_image_2') ?>'); ">
+<div class="hero-section life-section" id="life-section-2">
+    <div class="sect-background" style="background-image: url('<?php the_field('life_image_2') ?>'); "></div>
     <div class="hero-content animated">
         <h2><?php the_field('life_title_2'); ?></h2>
-        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/arrow_white.svg" /></a>
+        <div>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_white.svg" />
+        </div>
+        <a class="life-link" href="#"></a>
     </div>
 </div>
-<div class="hero-section" id="life-section-3" style="background-image: url('<?php the_field('life_image_3') ?>'); ">
+<div class="hero-section life-section" id="life-section-3">
+    <div class="sect-background" style="background-image: url('<?php the_field('life_image_3') ?>'); "></div>
     <div class="hero-content animated">
         <h2><?php the_field('life_title_3'); ?></h2>
-        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/arrow_white.svg" /></a>
+        <div>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_white.svg" />
+        </div>
+        <a class="life-link" href="#"></a>
     </div>
 </div>
-<div class="hero-section" id="life-section-4" style="background-image: url('<?php the_field('life_image_4') ?>'); ">
+<div class="hero-section life-section" id="life-section-4">
+    <div class="sect-background" style="background-image: url('<?php the_field('life_image_4') ?>'); "></div>
     <div class="hero-content animated">
         <h2><?php the_field('life_title_4'); ?></h2>
-        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/arrow_white.svg" /></a>
+       <div>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_white.svg" />
+        </div>
+        <a class="life-link" href="#"></a>
     </div>
 </div>
-<div class="hero-section" id="life-section-5" style="background-image: url('<?php the_field('life_image_5') ?>'); ">
+<div class="hero-section life-section" id="life-section-5" >
+    <div class="sect-background" style="background-image: url('<?php the_field('life_image_5') ?>'); "></div>
     <div class="hero-content animated">
         <h2><?php the_field('life_title_5'); ?></h2>
-        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/arrow_white.svg" /></a>
+        <div>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/arrow_white.svg" />
+        </div>
+        <a class="life-link" href="#"></a>
     </div>
 </div>
 <ul class="hero--pager">
@@ -59,6 +79,11 @@ get_header(); ?>
     <li class="pager--indicator">
         <a class="pager--indicator-link" href="#life-section-4">
             <span id="indicator-4"></span>
+        </a>
+    </li>
+    <li class="pager--indicator">
+        <a class="pager--indicator-link" href="#life-section-5">
+            <span id="indicator-5"></span>
         </a>
     </li>
 </ul>
