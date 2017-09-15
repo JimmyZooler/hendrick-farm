2b1429767087204171b227ca65246a3009d9898f
Craig <jimmy.zooler@gmail.com>
2017-09-15 17:12:54 -0400

    Updated version numbers.

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -13,7 +13,7 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		// Get the theme data.
 		$the_theme = wp_get_theme();
          wp_enqueue_style( 'magnific-styles', get_stylesheet_directory_uri() . '/src/css/magnific-popup.css', array() );
-		wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), '2.9' );
+		wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), '3.0' );
 //		wp_enqueue_script( 'jquery', get_template_directory_uri() . '/src/js/jquery.min.js');
 		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $the_theme->get( 'Version' ), true );
         wp_enqueue_script( 'waypoint-scripts', get_template_directory_uri() . '/src/js/jquery.waypoints.min.js', array('jquery'), $the_theme->get( 'Version' ), true );
@@ -22,7 +22,7 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
         wp_enqueue_style( 'animate-styles', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css');
         wp_enqueue_script( 'salvattore-scripts', get_stylesheet_directory_uri() . '/src/js/salvattore.min.js', array(), false, true );
         wp_enqueue_script( 'magnific-scripts', get_stylesheet_directory_uri() . '/src/js/jquery.magnific-popup.js', array(), false, true );
-        wp_enqueue_script( 'custom-scripts', get_template_directory_uri() . '/src/js/scripts.js', array('jquery'), '1.8', true );
+        wp_enqueue_script( 'custom-scripts', get_template_directory_uri() . '/src/js/scripts.js', array('jquery'), '1.9', true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
